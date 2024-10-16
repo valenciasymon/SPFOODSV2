3d6a8e22600504204a10c8f4b911ce0997d8b64c
valenciasymon <symon0515@gmail.com>
2024-10-16 18:59:41 +0800

    php and intent(start)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.android.volley:volley:1.2.1")
 }
--- app/src/main/java/com/example/spfood/MainActivity.java
+++ app/src/main/java/com/example/spfood/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.spfood;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    databaseHelper dbhelper = new databaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        dbhelper.addFood("sedap","noodles",35,"RANNEZ");
+        //connecting php script
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.54/test/index.php";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String fname = jsonObject.getString("food_name");
+                        String fprice = jsonObject.getString("food_price");
+                        String Sname = jsonObject.getString("store_name");
+
+                        //log
+                        Log.d("Item", "name: " + fname + ", price: " + fprice + ", Storename: " + Sname);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        },
+        new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //handling errors
+                Toast.makeText(getApplicationContext(),"failed to fetch data",Toast.LENGTH_SHORT).show();
+                Log.e("Volley Error",error.toString());
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
     }
 }
